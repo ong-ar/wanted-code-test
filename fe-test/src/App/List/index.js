1eb493732f5cb37c55b397b8824f70e0688bbea2
Hyunje Alex Moon <hyunje@wantedlab.com>
2018-12-19 22:56:54 +0900

    remove test code

--- fe-test/src/App/List/index.js
+++ fe-test/src/App/List/index.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 
 class List extends Component {
   render() {
-    const {
-      listData = [],
-    } = this.props;
-
     return(
       <div>
         List
