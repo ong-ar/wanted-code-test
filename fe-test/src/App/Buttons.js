62b8656cc77495640b243aa2239d1e80cefb2491
Hyunje Alex Moon <hyunje@wantedlab.com>
2018-12-24 11:58:38 +0900

    자세한 코드 설명 사항 추가

--- fe-test/src/App/Buttons.js
+++ fe-test/src/App/Buttons.js
@@ -4,8 +4,8 @@ class Buttons extends Component {
   render() {
     return(
       <div>
-        <button>GET JOBS</button>
-        <button>CLEAR JOBS</button>
+        <button>GET DOGS</button>
+        <button>CLEAR DOGS</button>
       </div>
     );
   }
